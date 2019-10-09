0512460ea42259746e00fd2a80026295ca61370b
std <std@admins-Mac-mini.local>
2019-10-09 14:41:29 -0700

    an error to debug and ask for help to resolve

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
+import Apod from "./Components/apod";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+  const [nasaData,setNasaData] = useState({});
+
+  useEffect( () => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .then(response => {
+        console.log("This is response:", response);
+        setNasaData(response);
+        console.log("This is nasaData in axios command:", nasaData);
+      })
+      .catch(error => {
+        console.log("The data was not returned",error);
+      });
+  },[]);
+
+  let dataL=nasaData.data;
+  console.log("The dataL is ",dataL);
+  // let imgUrl=dataL.hdurl;
+  // console.log("The imgUrl is ",imgUrl);
+
+
+  return <h3>Loading from if ...</h3>;
+
+  
+
 }
 
 export default App;
---
+++ src/Components/apod.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Apod({imgUrl,title,explanation})  {
+    return (
+        <div className="apod-card">
+            <h1>Nasa's Astronomy Picture of the day</h1>
+            <img alt="Nasa's astronomy picture" src={imgUrl} />
+            <h2>`Title of picture: ${title}`</h2>
+            <p>{explanation}</p>
+        </div>
+        
+    );
+  
+}
+  
